3f027355b4eaa31483ee839361114735f6b47f00
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-07-28 16:47:56 -0300

    transforming app component in just a const

--- applicantion/src/app.js
+++ applicantion/src/app.js
@@ -5,19 +5,18 @@ import database from './models'
 import dotenv from 'dotenv-safe'
 import cors from 'cors'
 
-class App {
-  constructor() {}
-  configureExpress() {
 
-    dotenv.config({
-      allowEmptyValues: true
-    })
-    const app = express()
-    app.use(bodyParser.json())
-    app.use(cors())
-    app.use('/', routes)
-    return app
-  }
+const configureExpress = () => {
+
+  dotenv.config({
+    allowEmptyValues: true
+  })
+  const app = express()
+  app.use(bodyParser.json())
+  app.use(cors())
+  app.use('/', routes)
+  return app
 }
 
-export default () => database.sequelize.sync({ force: false}).then(new App().configureExpress)
+
+export default () => database.sequelize.sync({ force: false}).then(configureExpress)
